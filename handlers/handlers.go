1f61234077fbb73ec594d33ed6754653f9e9cb9b
nicetrykiddo <bestxd916@gmail.com>
2026-01-04 19:02:00 +0530

    Remove message logger

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -41,7 +41,7 @@ func (h *Handler) HandleUpdate(update models.Update) {
 		return
 	}
 
-	log.Printf("Received message from %s: %s", msg.MessageID, msg.Text)
+	// log.Printf("Received message from %s: %s", msg.MessageID, msg.Text)
 
 	if strings.HasPrefix(msg.Text, "/") {
 		h.handleCommand(msg)
